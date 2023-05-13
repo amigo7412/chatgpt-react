a425705e89f5c6cace72a2880e96faed5ef49d1a
Popcorn412 <felix44291@gmail.com>
2023-05-13 12:20:06 -0500

    Initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
+
+require('dotenv').config();
+
+const configuration = new Configuration({
+	organization: process.env.REACT_APP_OPENAI_ORG_ID,
+	apiKey: process.env.REACT_APP_OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [msg, setMsg] = useState<string>('')
+	const [chats, setChats] = useState<string[]>([])
+	const [isTyping, setIsTyping] = useState<boolean>(false)
+
+	const chat = async (e: any, msg: string) => {
+		e.preventDefault();
+
+		if (!msg) {
+			setIsTyping(true);
+		}
+
+		await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: [{ role: 'user', content: msg }]
+		}).then((res: any) => {
+			setChats([...chats, res.data.choices[0]?.message.content]);
+			setMsg('');
+		}).catch((e) => {
+			console.log(e);
+		})
+	}
+
+	return (
+		<div>
+			<div>
+				{isTyping && <p>Typing...</p>}
+			</div>
+			<form action="" onSubmit={(e) => chat(e, msg)}>
+				<input
+					type="text"
+					placeholder='Input a message'
+					onChange={(e) => setMsg(e.target.value)}
+				/>
+			</form>
+		</div>
+	);
 }
 
 export default App;
